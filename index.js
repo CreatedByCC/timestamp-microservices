865fc77d054cda55a6870ec2ab7f7e6f7b5411e3
CC <chantelh88@gmail.com>
2024-12-10 22:07:52 +1300

    Completed steps 1 to 8 of the project

--- index.js
+++ index.js
@@ -21,10 +21,31 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+// TASK 2-8
+app.get("/api/:date?", function (req, res) {
+  const dateString = req.params.date;
+  let date;
+  
+  // Check if a date is provided else use current date
+  if (dateString) {
+    date = isNaN(dateString) ? new Date(dateString) : new Date(parseInt(dateString));
+  } else {
+    date = new Date();
+  }
 
+  // Check if date is valid
+  if(isNaN(date)) {
+    return res.json({ error : "Invalid Date" })
+  } 
+  
+  const timestamp = date.getTime();
+  const formattedDate = date.toUTCString();
+  
+  res.json({ unix: timestamp, utc: formattedDate });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
